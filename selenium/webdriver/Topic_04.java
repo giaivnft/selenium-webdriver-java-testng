387052ff490225b8e0475fb0502ee4125e81a3a6
giaivnft <giaidung@gmail.com>
2023-07-30 23:15:55 +0700

    Edit testcase 01

--- selenium/webdriver/Topic_04.java
+++ selenium/webdriver/Topic_04.java
@@ -5,6 +5,12 @@ import org.testng.annotations.Test;
 public class Topic_04 {
     @Test
     public void TC_01(){
-        //......
+        // Edit this line
     }
+
+    @Test
+    public void TC_02() {
+        // ....
+    }
+
 }
